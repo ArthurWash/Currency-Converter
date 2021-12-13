557382e495e366175171c28b66b9caa9bc96f613
AJ <ajw53@NJIT.edu>
2021-12-13 17:11:32 -0500

    Feature: Currency converter class created

--- Currency-Converter.py
+++ Currency-Converter.py
@@ -1,1 +1,21 @@
 import requests
+
+class Currency_converter:
+    # empty () to store the conversion rates
+    rates = {}
+
+    def __init__(self, url):
+        data = requests.get(url).json()
+
+        # Extracting only the rates from the json data
+        self.rates = data["rates"]
+
+    # Cross multiplication function
+    def convert(self, from_currency, to_currency, amount):
+        initial_amount = amount
+        if from_currency != 'USD' :
+            amount = amount / self.rates[from_currency]
+
+        # Limit to 2 decimal places
+        amount = round(amount * self.rates[to_currency], 2)
+        print('{} {} = {} {}'.format(initial_amount, from_currency, amount, to_currency))
